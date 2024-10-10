0caef75aee0f5340d1a85651655cb76fd7c675d1
Eirik Stanghelle Morland <eirik@morland.no>
2024-10-10 12:31:40 +0200

    Update ProcessFactory.php

--- src/ProcessFactory.php
+++ src/ProcessFactory.php
@@ -7,7 +7,7 @@ use Violinist\ProcessFactory\ProcessFactoryInterface;
 
 class ProcessFactory implements ProcessFactoryInterface
 {
-    public function getProcess(array $command, ?string $cwd = null, ?array $env = null, $input = null, ?float $timeout = 60);
+    public function getProcess(array $command, ?string $cwd = null, ?array $env = null, $input = null, ?float $timeout = 60)
     {
         return new Process($commandline, $cwd, $env, $input, $timeout);
     }
